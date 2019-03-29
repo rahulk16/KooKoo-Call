e43a84697ed67c46c9980c287d0c50c9cb472f35
rahul <rahul162rk@gmail.com>
2019-03-29 18:41:15 +0530

    Final conditions removed

--- util/kookoo.js
+++ util/kookoo.js
@@ -48,25 +48,25 @@ module.exports = {
     					]}]
     				};
 
-            if(data){
-              var age = parseInt(data);
-              if(age>21){
-                res = {
-          				response:
-          				[{
-          					playtext: 'You are an ADULT.'
-          				}]
-          			};
-              }
-              else{
-                res = {
-          				response:
-          				[{
-          					playtext: 'Minors are NOT ALLOWED.'
-          				}]
-          			};
-              }
-            }
+            // if(data){
+            //   var age = parseInt(data);
+            //   if(age>21){
+            //     res = {
+          	// 			response:
+          	// 			[{
+          	// 				playtext: 'You are an ADULT.'
+          	// 			}]
+          	// 		};
+            //   }
+            //   else{
+            //     res = {
+          	// 			response:
+          	// 			[{
+          	// 				playtext: 'Minors are NOT ALLOWED.'
+          	// 			}]
+          	// 		};
+            //   }
+            // }
           }
           if(gender==2){ //female
             res = {
@@ -84,25 +84,25 @@ module.exports = {
     					]}]
     				};
 
-            if(data){
-              var age = parseInt(data);
-              if(age>18){
-                res = {
-          				response:
-          				[{
-          					playtext: 'You are an ADULT.'
-          				}]
-          			};
-              }
-              else{
-                res = {
-          				response:
-          				[{
-          					playtext: 'Minors are NOT ALLOWED.'
-          				}]
-          			};
-              }
-            }
+            // if(data){
+            //   var age = parseInt(data);
+            //   if(age>18){
+            //     res = {
+          	// 			response:
+          	// 			[{
+          	// 				playtext: 'You are an ADULT.'
+          	// 			}]
+          	// 		};
+            //   }
+            //   else{
+            //     res = {
+          	// 			response:
+          	// 			[{
+          	// 				playtext: 'Minors are NOT ALLOWED.'
+          	// 			}]
+          	// 		};
+            //   }
+            // }
 
           }
         }
